3a9060cfb45d3e590868cef881311be4c341b908
NeoGendaijin <aming4mit@gmail.com>
2023-10-09 16:35:46 +0900

    quick_start.py miner change

--- quick_start.py
+++ quick_start.py
@@ -1,8 +1,7 @@
-
 from src.main import download_relevent_papers
 
 
 # How to use
 your_question = "What is the best prompt to enhanse LLM's math ability?"
-save_dir = "./pdf1"
+save_dir = "./pdfs"
 download_relevent_papers(your_question, save_dir = save_dir)
